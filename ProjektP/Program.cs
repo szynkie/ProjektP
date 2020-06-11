62c75a4f3d38ce0e9b374aa0908f835cd08c6471
BuildTools <szynekq@gmail.com>
2020-06-11 20:14:11 +0200

    Add project files.

---
+++ ProjektP/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjektP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Wpisz imie:");
+            string userName = Console.ReadLine();
+            Console.WriteLine($"Witaj {userName}");
+            Console.ReadKey();
+        }
+    }
+}
