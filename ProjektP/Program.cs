654b936d458b56543207a5fd49ee1c9c67d94103
dominiksuski <szynekq@gmail.com>
2020-06-12 20:33:59 +0200

    Menu oraz zarys wyboru postaci

--- ProjektP/Program.cs
+++ ProjektP/Program.cs
@@ -4,12 +4,24 @@ namespace ProjektP
 {
     class Program
     {
+        public static int Characters()
+        {
+            int characterSelector = 0;
+            
+            return characterSelector;
+        }
         static void Main(string[] args)
         {
             Console.WriteLine("Wpisz imie:");
             string userName = Console.ReadLine();
             Console.WriteLine($"Witaj {userName}");
-            Console.ReadKey();
+
+            Console.WriteLine($"Menu glowne \n 1.Wybor postaci \n 2.xxx \n 3.xxx");
+            int menuSelector = Int32.Parse(Console.ReadLine());
+            switch (menuSelector)
+            {
+                
+            }
         }
     }
 }
