4957983838e88ce32e476b41efc60a5afe2ff6cf
dominiksuski <szynekq@gmail.com>
2020-06-16 23:16:46 +0200

    Dodana logika papier, kamien, nozyce i poprawki

--- ProjektP/Program.cs
+++ ProjektP/Program.cs
@@ -6,9 +6,19 @@ namespace ProjektP
     {
         public static void Main(string[] args)
         {
+            Console.WriteLine("Wpisz nazwe gracza:");
+            Silnik.playerName = Console.ReadLine();
             Console.WriteLine($"Menu glowne \n 1.Wybor postaci \n 2.Wybor poziomu trudnosci \n 3.Start gry \nAby kontynuowac wcisnij klawisz");
             Console.ReadKey();
             Silnik.Player();
+        }
+
+        public static void Gameplay()
+        {
+            Console.WriteLine($"Witaj w grze papier, kamien, nozyce {Silnik.playerName}! Masz {Silnik.lifePoints} punktow zycia oraz {Silnik.attackPoints} punktow ataku." +
+                              $" \nTwoj przeciwnik ma {Silnik.monsterLife} punktow zycia oraz {Silnik.monsterAttack} punktow ataku" +
+                              $" \nPo kazdej wygranej turze otrzymasz dodatkowy punkt zycia");
+            Silnik.RPS();
         }
     }
 }
--- ProjektP/Silnik.cs
+++ ProjektP/Silnik.cs
@@ -1,17 +1,19 @@
 using System;
-using System.Reflection;
-using System.Runtime.ConstrainedExecution;
 
 namespace ProjektP
 {
     public class Silnik
     {
         public static int lifePoints = 0;
+        public static int attackPoints = 0;
+        public static int monsterLife = 0;
+        public static int monsterAttack = 0;
+        public static string playerName = "";
         public static void Player()
         {
             int playerId = 0;
             //int lifePoints;
-            int attackPoints;
+            //int attackPoints;
             Console.WriteLine("Wybierz postac: \n 1.Mnich \n 2.Nekromanta");
             playerId = Int32.Parse(Console.ReadLine());
             switch (playerId)
@@ -31,8 +33,8 @@ namespace ProjektP
         public static void Monster()
         {
             int monsterId = 0;
-            int monsterLife;
-            int monsterAttack;
+            //int monsterLife;
+            //int monsterAttack;
             Console.WriteLine("Wybierz przeciwnika: \n 1.Bezlebub(latwy) \n 2.Lucyfer(trudny)");
             monsterId = Int32.Parse(Console.ReadLine());
             switch (monsterId)
@@ -47,12 +49,85 @@ namespace ProjektP
                     break;
             }
 
-            RPS();
+            Program.Gameplay();
         }
 
         public static void RPS()
         {
-            Console.WriteLine(lifePoints);
+            while (lifePoints > 0 && monsterLife > 0)
+            {
+                Console.WriteLine("Wpisz: \nPapier \nKamien \nNozyce");
+                string[] choices = new string[3] {"PAPIER", "KAMIEN", "NOZYCE"};
+                Random random = new Random();
+                int i = random.Next(0, 3);
+                //var playerChoice = Console.ReadLine();
+                string playerChoice = Console.ReadLine().ToUpper();
+                Console.WriteLine($"{playerName}: {playerChoice}");
+                Console.WriteLine("Przeciwnik: " + choices[i]);
+
+                if (playerChoice == "KAMIEN" && choices[i] == "NOZYCE")
+                {
+                    Console.WriteLine($"{playerName} wygrywa!");
+                    monsterLife = monsterLife - attackPoints;
+                    lifePoints++;
+                    Console.WriteLine($"{playerName} ma {lifePoints} punktow zycia" +
+                                      $" \nPrzeciwnik ma {monsterLife} punktow zycia");
+                }
+                else if (playerChoice == "KAMIEN" && choices[i] == "PAPIER")
+                {
+                    Console.WriteLine("Przeciwnik wygrywa!");
+                    lifePoints = lifePoints - monsterAttack;
+                    monsterLife++;
+                    Console.WriteLine($"{playerName} ma {lifePoints} punktow zycia" +
+                                      $" \nPrzeciwnik ma {monsterLife} punktow zycia");
+                }
+                else if (playerChoice == "PAPIER" && choices[i] == "KAMIEN")
+                {
+                    Console.WriteLine($"{playerName} wygrywa!");
+                    monsterLife = monsterLife - attackPoints;
+                    lifePoints++;
+                    Console.WriteLine($"{playerName} ma {lifePoints} punktow zycia" +
+                                      $" \nPrzeciwnik ma {monsterLife} punktow zycia");
+                }
+                else if (playerChoice == "PAPIER" && choices[i] == "NOZYCE")
+                {
+                    Console.WriteLine("Przeciwnik wygrywa!");
+                    lifePoints = lifePoints - monsterAttack;
+                    monsterLife++;
+                    Console.WriteLine($"{playerName} ma {lifePoints} punktow zycia" +
+                                      $" \nPrzeciwnik ma {monsterLife} punktow zycia");  
+                }
+                else if (playerChoice == "NOZYCE" && choices[i] == "KAMIEN")
+                {
+                    Console.WriteLine($"{playerName} wygrywa!");
+                    monsterLife = monsterLife - attackPoints;
+                    lifePoints++;
+                    Console.WriteLine($"{playerName} ma {lifePoints} punktow zycia" +
+                                      $" \nPrzeciwnik ma {monsterLife} punktow zycia");
+                }
+                else if (playerChoice == "NOZYCE" && choices[i] == "PAPIER")
+                {
+                    Console.WriteLine("Przeciwnik wygrywa!");
+                    lifePoints = lifePoints - monsterAttack;
+                    monsterLife++;
+                    Console.WriteLine($"{playerName} ma {lifePoints} punktow zycia" +
+                                      $" \nPrzeciwnik ma {monsterLife} punktow zycia");  
+                }
+                else
+                {
+                        Console.WriteLine("Remis!");
+                }
+                /*Console.WriteLine($"{playerName} ma {lifePoints} punktow zycia" +
+                                  $" \nPrzeciwnik ma {monsterLife} punktow zycia");*/
+                if (lifePoints <= 0)
+                {
+                    Console.WriteLine("Przeciwnik zwyciezyl!");
+                }
+                else if (monsterLife <= 0)
+                {
+                    Console.WriteLine($"{playerName} zwyciezyl!");
+                }
+            }
         }
     }
 }
